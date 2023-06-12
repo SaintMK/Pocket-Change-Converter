3a666af098a85ebcd62bda147854b092443e36f6
Michael Klumpp <19579509+SaintMK@users.noreply.github.com>
2023-06-12 17:22:48 -0400

    Add project files.

---
+++ Pocket Change Converter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PocketChange
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pocket Change");
+
+            string userInput = Console.ReadLine();
+            int userInputInt = Convert.ToInt32(userInput);
+
+            int silverValue = 5;
+            int goldValue = 10;
+
+            double numGoldCoins = 0;
+            double numSilverCoins = 0;
+            double numBronzeCoins = 0;
+
+            if (userInputInt >= 10)
+            {
+                numGoldCoins = userInputInt / goldValue;
+                Math.Floor(numGoldCoins);
+            }
+
+            if (numGoldCoins > 0)
+            {
+                numSilverCoins = Math.Floor(((userInputInt) - numGoldCoins * goldValue) / silverValue);
+            }
+
+            if (numSilverCoins > 0)
+            {
+                numBronzeCoins = userInputInt % silverValue;
+            }
+
+            Console.WriteLine($"You have {numGoldCoins} gold coins");
+            Console.WriteLine($"You have {numSilverCoins} silver coins");
+            Console.WriteLine($"You have {numBronzeCoins} bronze coins");
+        }
+
+    }
+}
